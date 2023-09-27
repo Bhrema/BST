b4579ff23143969b42f62c917ef3ac0d706b17be
Bruno Henrique Crema <brunohcrema@gmail.com>
2023-09-27 08:30:09 -0300

    first commit

---
+++ src/BuscaBinaria.java
@@ -0,0 +1,21 @@
+public class BuscaBinaria implements ArvoreBinariaInterface {
+    @Override
+    public Node PegaRoot() {
+        return null;
+    }
+
+    @Override
+    public Node BuscaNode(int key) {
+        return null;
+    }
+
+    @Override
+    public void InsereNode(int key) {
+
+    }
+
+    @Override
+    public void DeletaNode(int key) {
+
+    }
+}
