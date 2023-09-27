7090fa8d75d20b0e6c1bbb96f5fb1b9ad35381b2
Bruno Henrique Crema <brunohcrema@gmail.com>
2023-09-27 10:41:23 -0300

    TODO para o main

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         BuscaBinaria BB = new BuscaBinaria();
+        System.out.println("Entre com um numero na árvore: ");
+        //TODO fazer um while pra ficar perguntando até digitar 'f'
         Scanner scanner = new Scanner(System.in);
         int numero = scanner.nextInt();
-        System.out.println("Entre com um numero na árvore: ");
         BB.InsereNode(numero);
-
+        System.out.println("\nTree: " + BB);
     }
 }
